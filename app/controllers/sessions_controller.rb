1d83e7e36420e974d1ad04e61bf7b4f770a80df3
carly <carly@noukod.com>
2019-03-26 13:20:36 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,13 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-  foursquare = FoursquareService.new
-  session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
-  redirect_to root_path
-end
-
-    body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
+    foursquare = FoursquareService.new
+    session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], params[:code])
     redirect_to root_path
   end
 
-
+  def friends
+      foursquare = FoursquareService.new
+      @friends = foursquare.friends(session[:token])
+  end
 end
